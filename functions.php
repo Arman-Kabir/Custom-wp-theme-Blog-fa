515c25756f391f02a570d1f44bf953bf2430c4ff
Arman-Kabir <armankabir8@gmail.com>
2023-03-23 19:21:02 +0600

    my-css file added

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 
 function followandrew_register_styles(){
-    wp_enqueue_style('followandrew-bootstrap',get_template_directly_uri()."/style.css",array(),'1.0','all');
+    wp_enqueue_style('followandrew-bootstrap',get_template_directory_uri()."/style.css",array(),'1.0','all');
 }
 
 add_action('wp_enqueue_scripts','followandrew_register_styles');
-// 
-?>
+ 
+?> 
