1b4dc73ef3ebcfeb8f32775b9520bf457a9ad49c
Arman-Kabir <armankabir8@gmail.com>
2023-03-24 16:46:50 +0600

    title added

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+function followandrew_theme_support()
+{
+    // adds dynamic title tag support
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme','followandrew_theme_support');
+
 function followandrew_register_styles()
 {
 
@@ -17,16 +25,16 @@ add_action('wp_enqueue_scripts', 'followandrew_register_styles');
 
 function followandrew_register_scripts()
 {
-    wp_enqueue_script('followandrew-jquery','https://code.jquery.com/jquery-3.4.1.slim.min.js',array(),'3.4.1',true);
+    wp_enqueue_script('followandrew-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
 
-    wp_enqueue_script('followandrew-popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',array(),'1.16.0',true);
+    wp_enqueue_script('followandrew-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
 
-    wp_enqueue_script('followandrew-bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',array(),'3.4.1',true);
+    wp_enqueue_script('followandrew-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '3.4.1', true);
 
-    wp_enqueue_script('followandrew-main',get_template_directory_uri()."/assets/js/main.js",array(),'3.4.1',true);
-    
+    wp_enqueue_script('followandrew-main', get_template_directory_uri() . "/assets/js/main.js", array(), '3.4.1', true);
 
-    
+
+
 }
 
 add_action('wp_enqueue_scripts', 'followandrew_register_scripts');
