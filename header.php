8565ea967a7566197b5aa470581af4cfe7ce7bbf
Arman-Kabir <armankabir8@gmail.com>
2023-03-25 00:49:28 +0600

    logo.png

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">
-    <link rel="shortcut icon" href="images/logo.png">
+    <link rel="shortcut icon" href="./wp-content/themes/followandrew/assets/images/logo.png">
 
     <!-- FontAwesome CSS-->
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"> -->
